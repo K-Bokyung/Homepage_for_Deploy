1478ccfa5b7e3b1c901759ee7393b8b3f8d8e8db
Bokyung Kwon <tree445560@gmail.com>
2024-11-28 11:50:52 +0900

    edit: repo 이름 변경

--- home_front/src/main.jsx
+++ home_front/src/main.jsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='Homepage_for_Deploy/'>
+    <BrowserRouter basename='Training_for_Deploy/'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
